a54db41505570ed6a0c5b225d4cf36fc2a6c4dd9
yassine2734 <elmaghraouiyassine3@gmail.com>
2026-03-12 10:41:41 +0100

    Fix Next export for /lequipe

--- lequipe/public/_next/static/0tXPPMDYRYni3dD1CxmUT/_buildManifest.js
+++ lequipe/public/_next/static/0tXPPMDYRYni3dD1CxmUT/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/lequipe//_next/:path+",
+        "destination": "/lequipe/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
